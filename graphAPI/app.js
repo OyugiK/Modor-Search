b7728eb3423cb67ef3a83afc4b5439e4c940a2d1
OyugiK <Oyugi.K.Owino@gmail.com>
2015-03-09 14:10:00 +0300

    API Authentification

--- graphAPI/app.js
+++ graphAPI/app.js
@@ -15,8 +15,9 @@ c) Mobile (Android, Iphone, Nokia)
 
 #TODO ::::
 Add Tokenisation
-Add Logging
-Add Authentification
+
+# Auth will be Basic HTTP Auth
+# Served over HTTPs
 
 
 
@@ -33,7 +34,62 @@ var app = express();
 app.engine('hbs', expressHbs({extname:'hbs', defaultLayout:'main.hbs'}));
 app.set('view engine', 'hbs');
 
+logger.info("authenticating api users");
+
+// passport
+var passport = require("passport")
+	, LocalStrategy = require('passport-local').Strategy;
+
+// password auth
+passport.use(new LocalStrategy(
+	function(username, password, done) {
+		logger.info("authenticating user %s", username);
+		// use db username and password auth here
+		// future faetures
+		if(username == "87bf83fc" && password == "f4a1112a"){
+			logger.info("auth OK %s", username);
+			return done(null, {login : 100, id : 4});
+		}
+		else{
+			logger.info("auth Fail %s", username);
+			return done(null, false, { message: 'Bad Credentials'});
+		}
+	}
+));
+
+
+// function to serialize logged in user
+passport.serializeUser(function(user, done) {
+	logger.info("serializing user %s", JSON.stringify(user));
+	done(null, user.id);
+});
+// function to de serialize logged in user
+passport.deserializeUser(function(id, done) {
+	logger.info("de-serializing user with id %s", id);
+	/*User.findById(id, function(err, user) {
+	done(err, user);
+	});*/
+	return done(null, {login : 100, id : 4});
+});
+
+// set the app use
+app.use(passport.initialize());
+app.use(passport.session());
+
+// authenticate incoming requests
+app.use(passport.authenticate('local'), function(req, res, next){
+next();
+});
+
+
 logger.info("calling the graph api");
+
+// log incoming requests
+app.use(function(req, res, next){
+logger.info('%s %s %s <- %s', req.protocol, req.method, req.url, req.ip);
+next();
+});
+
 // instantiate the people graph api
 var api = new graphAPI();
 
